f0eb0eb21a1bc04d770817fc4e5ca5c7200a88f0
guanlingc <141930084+guanlingc@users.noreply.github.com>
2024-11-28 23:05:57 +0800

    Create day1_project.py
    Finished project of day 1 script

---
+++ day1_project.py
@@ -0,0 +1,8 @@
+# Create a greeting for your program.
+print(f'Hello!! Welcome to Band Name Generator')
+# Ask the user for the city that they grew up in and store it in a variable.
+city = input("Which city did you grew up in?\n")
+# Ask the user for the name of a pet and store it in a variable.
+pet_name = input("What is the name of your pet?\n")
+# Combine the name of their city and pet and show them their band name.
+print(f'Your Band Name is {city} {pet_name}!!!')
