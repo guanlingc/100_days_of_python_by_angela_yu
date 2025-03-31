607b55952806cbe22b5497e48c9442e0d206838c
Chan Guan Ling <chan.guanling@gmail.com>
2025-03-31 23:31:49 +0800

    housekeeping of code

--- Intermediate_Day16_to_Day31/Day_16/day16_project.py
+++ Intermediate_Day16_to_Day31/Day_16/day16_project.py
@@ -7,6 +7,8 @@ from resources.money_machine import MoneyMachine
 
 def run_coffee_machine():
     menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
     machine_is_running = True
 
     while machine_is_running:
@@ -15,20 +17,16 @@ def run_coffee_machine():
             print(f"shutting down machine for maintenance")
             machine_is_running = False
         elif coffee_choice == 'report':
-            coffee_maker = CoffeeMaker()
             coffee_maker.report()
+            money_machine.report()
         else:
             drink = menu.find_drink(coffee_choice) # use menu to get drink's attributes
-            # print(drink.name,drink.cost,drink.ingredients)
-            coffee_maker = CoffeeMaker()
+            # print(drink.name,drink.cost,drink.ingredients)            
             if coffee_maker.is_resource_sufficient(drink):
-                money_machine = MoneyMachine()
                 if money_machine.make_payment(drink.cost):
                     coffee_maker.make_coffee(drink)
-                else:
-                    pass
             else:
-                pass
+                print(f"Sorry, we don't serve {coffee_choice}.")
             
 if __name__ == "__main__":
     run_coffee_machine()
